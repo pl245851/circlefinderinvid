45a8958c518cb241a29367920f7a91b4380ae0f4
pl245851 <70595860+pl245851@users.noreply.github.com>
2021-04-12 09:20:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
+import random
 
-cap = cv2.VideoCapture("Rolling.MOV")
+cap = cv2.VideoCapture("IMG_9803.MOV")
 ret, frame = cap.read()
 while(1):
     #print(ret, frame)
@@ -11,10 +12,10 @@ while(1):
         break
     output = frame.copy()
     blur = cv2.GaussianBlur(frame, (5, 5), 0)
-    nframe = cv2.convertScaleAbs(blur, alpha=2.0, beta=10)
-    gray = cv2.cvtColor(nframe, cv2.COLOR_BGR2GRAY)
-    gray[0:150, 0:1280] = np.ones((150,1280))
-    gray[300:720, 0:1280] = np.ones((420, 1280))
+    #nframe = cv2.convertScaleAbs(blur, alpha=2.0, beta=10)
+    gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+    #gray[0:100, 0:1280] = np.ones((100,1280))
+    #gray[300:720, 0:1280] = np.ones((420, 1280))
     cv2.imshow("",gray)
     #blur = cv2.GaussianBlur(gray, (5, 5), 0)
     circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
@@ -29,11 +30,12 @@ while(1):
                 maxx = x
                 maxy = y
                 maxr = r
-                cv2.circle(output, (x, y), r, (0, 165, 255), 4)
-                cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (50, 50, 50), -1)
+        #cv2.circle(output, (maxx, maxy), maxr, (0, 165, 255), 4)
+        cv2.circle(output, (maxx, maxy), maxr, (random.randrange(0,255), random.randrange(0,255), random.randrange(0,255)), 4)
+        cv2.rectangle(output, (maxx - 5, maxy - 5), (maxx + 5, maxy + 5), (50, 50, 50), -1)
 
     cv2.imshow('frame',np.hstack([frame, output]))
-    if cv2.waitKey(0) & 0xFF == ord('q') or ret==False :
+    if cv2.waitKey(1) & 0xFF == ord('q') or ret==False :
        cap.release()
        cv2.destroyAllWindows()
        break
